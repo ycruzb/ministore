bb9718bc863059dc7005330e8ae1a5cea83ed0e5
ycruz-webimpacto <ycruz-webimpacto@webimpacto.es>
2021-12-07 17:58:42 -0500

    first commit: add base starter code

---
+++ layouts/layout.js
@@ -0,0 +1,14 @@
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Header />
+			{children}
+			<Footer />
+		</>
+	)
+}
+
+export default Layout
---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/tailwind.css'
+import Layout from '../layouts/layout'
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	)
+}
+export default MyApp
