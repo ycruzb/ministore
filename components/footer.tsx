7d66fb089ce5779569c289d51ca6869884ddb8b2
ycruz-webimpacto <ycruz-webimpacto@webimpacto.es>
2021-12-07 19:19:22 -0500

    add: adding heroicons module, adding base structure to the header and footer, styling header and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,7 @@
 const Footer = () => {
-	return <footer className="text-xl">Footer</footer>
+	return <footer className="w-full text-center ">
+		<p className="text-sm text-gray-400 px-5 py-6">&copy; 2021 Ministore</p>
+	</footer>
 }
 
 export default Footer
--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link"
+import { ShoppingCartIcon } from "@heroicons/react/solid"
+
 const Header = () => {
-	return <header className="text-xl">Header</header>
+	return <header className="w-full px-5 py-4 flex justify-between">
+		<div className="">
+			<Link href="/"><a className="text-gray-800 text-2xl block">Ministore</a></Link>
+		</div>
+		<div>
+			<ShoppingCartIcon className="text-gray-800 w-6 h-6 mt-[6px]" />
+		</div>
+	</header>
 }
 
 export default Header
