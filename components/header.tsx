1f32e0f4218b58fae488fcac97bae3cf195b94ae
ycruz-webimpacto <ycruz-webimpacto@webimpacto.es>
2021-12-08 15:04:02 -0500

    add: adding sticky effect to the header, applying according styles to the header

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import { ShoppingCartIcon } from "@heroicons/react/solid"
+import useScrollPosition from "../hooks/useScrollPosition"
 
 const Header = () => {
-	return <header className="w-full px-5 py-4 flex justify-between border-b border-gray-200">
+	const scrollPosition = useScrollPosition();
+	const headerClass = scrollPosition < 10 ? `w-full px-5 py-4 flex justify-between border-b border-gray-200 sticky top-0 z-50 bg-white` : `w-full px-5 py-4 flex justify-between border-b border-gray-200 sticky top-0 z-50 bg-white/60 transition duration-200 backdrop-filter backdrop-blur-lg shadow-lg`
+	return <header className={headerClass}>
 		<div className="">
 			<Link href="/"><a className="text-2xl block">Ministore</a></Link>
 		</div>
